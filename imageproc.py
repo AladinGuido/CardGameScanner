e85c5751689f954c8bc952c07182ef78bc458f14
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-05-06 18:31:22 +0800

    added function to reorder the points to always get an upright image added function to warp picture

--- imageproc.py
+++ imageproc.py
@@ -1,23 +1,83 @@
+import copy
+
 import cv2
 import numpy as np
 
+def get_coordinate_diff(c1, c2):
+    x1 = c1[0]
+    y1 = c1[1]
+
+    x2 = c2[0]
+    y2 = c2[1]
+
+    x_diff_squared = (x1 - x2)**2
+    y_diff_squared = (y1 - y2)**2
+    distance = (x_diff_squared + y_diff_squared)**(1/2)
+    return distance
 
 class ImageProcessing:
 
     def __init__(self):
         self.KERNEL_SIZE = np.ones((5, 5))
         self.THRESHOLD_ADD = 100
+        self.MIN_AREA_SIZE = 5000
+        self.CARD_HEIGHT = int(1028*0.75)
+        self.CARD_WIDTH = int(661*0.75)
 
     def pre_process_image(self, image):
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image_blurred = cv2.GaussianBlur(image_gray, (5, 5), 0)
         image_canny = cv2.Canny(image_blurred, 100, 150)
-        #image_dilate = cv2.dilate(image_canny, self.KERNEL_SIZE, iterations=2)
-        #image_erode = cv2.erode(image_dilate, self.KERNEL_SIZE, iterations=1)
+        # image_dilate = cv2.dilate(image_canny, self.KERNEL_SIZE, iterations=2)
+        # image_erode = cv2.erode(image_dilate, self.KERNEL_SIZE, iterations=1)
 
         return image_canny
 
-    def get_corners(self, pre_processed_image):
-        cnts, hier = cv2.findContours(pre_processed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        index_sort = sorted(range(len(cnts)), key=lambda i: cv2.contourArea(cnts[i]), reverse=True)
-        print(index_sort)
+    def get_biggest_contour(self, pre_processed_image):
+        biggest_area = -1
+        biggest_contour = []
+        contours, hier = cv2.findContours(pre_processed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            contour_area = cv2.contourArea(contour)
+            if contour_area > self.MIN_AREA_SIZE:
+                if contour_area > biggest_area:
+                    biggest_area = contour_area
+                    biggest_contour = contour
+        return biggest_contour
+
+    def get_corners(self, contour):
+        arc_length = cv2.arcLength(contour, True)
+        approximation = cv2.approxPolyDP(contour, 0.05 * arc_length, True)
+        if len(approximation) == 4:
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            return approximation, box
+        return []
+
+    def reorder_corners(self, corners):
+        ordered_points = np.zeros((4, 1, 2), np.int32)
+        corner_points_sum = corners.sum(1)
+
+        ordered_points[0] = corners[np.argmin(corner_points_sum)]
+
+
+        diff_dictionary = {}
+        for point in corners:
+            coordinate_diff = get_coordinate_diff(point, ordered_points[0][0])
+            if coordinate_diff != 0.0:
+                diff_dictionary[coordinate_diff] = point
+
+        for i, key in enumerate(sorted(diff_dictionary.keys())):
+            ordered_points[(i+1)] = diff_dictionary.get(key)
+
+
+        return ordered_points
+
+    def warp_image(self, image, corners):
+        source_points = np.float32(corners)
+        destination_points = np.float32([[0, 0], [self.CARD_WIDTH, 0], [0, self.CARD_HEIGHT], [self.CARD_WIDTH, self.CARD_HEIGHT]])
+        perspective_matrix = cv2.getPerspectiveTransform(source_points, destination_points)
+        warped_image = cv2.warpPerspective(image, perspective_matrix, (self.CARD_WIDTH, self.CARD_HEIGHT))
+        return warped_image
+
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+import copy
+from urllib import request
+import webbrowser
 import cv2
-
+import numpy as np
 import imageproc
 
-CAMERA_URI = "http://192.168.1.238:4747/video"
+CAMERA_URI = "http://192.168.1.238:4747/video"#?1920x1080"
+
 
 def main():
-
     image_processing = imageproc.ImageProcessing()
 
     capture = cv2.VideoCapture(CAMERA_URI)
     while (True):
+        # get image from ip cam
         ret, frame = capture.read()
-        output_frame = image_processing.pre_process_image(image=frame)
-        image_processing.get_corners(output_frame)
+        frame_show = copy.copy(frame)
 
-        cv2.imshow('frame', output_frame)
+        # preprocess image and get largest contour
+        output_frame = image_processing.pre_process_image(image=frame)
+        contour = image_processing.get_biggest_contour(output_frame)
+        if len(contour) != 0:
+
+            # get corners of largest contour
+            approximation, corners = image_processing.get_corners(contour)
+
+
+            if len(corners) != 0:
+                reordered_approx = approximation.reshape((4,2))
+                reordered_corners = image_processing.reorder_corners(reordered_approx)
+                #frame_show = cv2.drawContours(frame_show, reordered_corners, -1, (0, 0, 255), 5)
+
+                frame = image_processing.warp_image(frame, reordered_corners)
+
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
